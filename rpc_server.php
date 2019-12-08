d8b6941fb135fd967b446092b35762b52d6f883e
benhuang <benhuang1024@gmail.com>
2019-12-08 20:07:04 +0800

    update run function

--- rpc_server.php
+++ rpc_server.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * User: Ben
- * Date: 19/11/12
- * Time: 14:00
- */
 
 require_once __DIR__ . '/vendor/autoload.php';
-
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
@@ -15,42 +9,38 @@ $channel = $connection->channel();
 
 $channel->queue_declare('rpc_queue', false, false, false, false);
 
-function fib($n)
-{
-    if ($n == 0) {
+function fib($n) {
+    if ($n == 0)
         return 0;
-    }
-    if ($n == 1) {
+    if ($n == 1)
         return 1;
-    }
-    return fib($n - 1) + fib($n + 1);
+    return fib($n-1) + fib($n-2);
 }
 
 echo " [x] Awaiting RPC requests\n";
-$callback = function ($req) {
+$callback = function($req) {
     $n = intval($req->body);
-    echo '[.] fib(' . $n . ')\n';
+    echo " [.] fib(", $n, ")\n";
 
     $msg = new AMQPMessage(
-        (string)fin($n), array('correlation_id' => $req->get('correlation_id'))
-    );
+        (string) fib($n),
+        array('correlation_id' => $req->get('correlation_id'))
+        );
 
     $req->delivery_info['channel']->basic_publish(
-        $msg,
-        '',
-        $req->get('reply_to')
-    );
+        $msg, '', $req->get('reply_to'));
     $req->delivery_info['channel']->basic_ack(
-        $req->delivery_info['delivery_ta']
-    );
+        $req->delivery_info['delivery_tag']);
 };
 
 $channel->basic_qos(null, 1, null);
 $channel->basic_consume('rpc_queue', '', false, false, false, false, $callback);
 
-while ($channel->is_consuming()) {
+while(count($channel->callbacks)) {
     $channel->wait();
 }
 
 $channel->close();
 $connection->close();
+​
+?>
